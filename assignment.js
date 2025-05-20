41fda262e89ddfa759e3e7a1421b70f32b682071
ayomide <sulaimanayomide005@gmail.com>
2025-05-20 13:15:19 +0100

    assignment answers updated

--- assignment.js
+++ assignment.js
@@ -1,13 +1,12 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
-if (randomNumber > 0.7) {
-    alert("The Number is definitely less than 0.7")
-}
-console.log(randomNumber);
+// if (randomNumber > 0.7) {
+//     // alert("The Number is definitely less than 0.7")
+// }
 
 
 
 
-let randomFigures = [3,2,1,4,5,4]
+let randomFigures = [3,2,1,4,5,4,8]
 let allNumbers = ''
 for (let i = 0; i < randomFigures.length; i++) {
     if(!allNumbers) {
@@ -20,11 +19,17 @@ console.log(allNumbers);
 
 let allDigits = ''
 for (const lastFig of randomFigures) {
-    // if (!allDigits) {
-    //     allDigits = lastFig
-    // } else {
-    //     allDigits += ", " + lastFig;
-    // }
-    allDigits = `${randomFigures}`
+    if (!allDigits) {
+        allDigits = lastFig
+    } else {
+        allDigits = `${lastFig}, ${allDigits}`
+    }
 }
 console.log(allDigits);
+
+const secRandomNumber = Math.random()
+if ((randomNumber > 0.7 && secRandomNumber > 0.7) || (randomNumber <= 0.2 || secRandomNumber <= 0.2)) {
+    alert("Numbers are either more than 0.7 or less than 0.2")
+    console.log(secRandomNumber);
+    console.log(randomNumber);
+}
